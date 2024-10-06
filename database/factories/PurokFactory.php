660f56c5e18060f96b1b1bde3b0e0b643bebd2e4
pseudocat <sydneyang1234@gmail.com>
2024-10-06 10:17:29 +0800

    enabled use HasFactory to Barangay and Purok models

---
+++ database/factories/PurokFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Purok;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PurokFactory extends Factory
+{
+    protected $model = Purok::class;
+
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+}
