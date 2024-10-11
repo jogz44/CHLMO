7cb4c3bd5b3a21cd3df882dcc4af2dbdae0278b0
pseudocat <sydneyang1234@gmail.com>
2024-10-11 18:51:32 +0800

    created Models for Shelter

---
+++ app/Models/Shelter/OriginOfRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models\Shelter;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class OriginOfRequest extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+    ];
+
+    public function shelterApplicants(): HasMany
+    {
+        return $this->hasMany(ShelterApplicant::class, 'request_origin_id');
+    }
+}
