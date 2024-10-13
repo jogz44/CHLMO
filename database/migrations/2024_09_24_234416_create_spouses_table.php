a9bdb668b36b094b3bc95931ee658dc24ea395b4
pseudocat <sydneyang1234@gmail.com>
2024-10-13 20:28:46 +0800

    renamed the spouses migration

---
+++ database/migrations/2024_09_24_234416_create_spouses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('spouses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tagged_and_validated_applicant_id')->constrained('tagged_and_validated_applicants')->onDelete('cascade');
+            $table->string('spouse_first_name', 50);
+            $table->string('spouse_middle_name', 50)->nullable();
+            $table->string('spouse_last_name', 50);
+            $table->string('spouse_occupation');
+            $table->integer('spouse_monthly_income');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('spouses');
+    }
+};
