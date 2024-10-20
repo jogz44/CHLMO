247f2fb20e5c0991f2c0aa340c8bfabb20d58690
pseudocat <sydneyang1234@gmail.com>
2024-10-20 10:56:52 +0800

    LotSizeUnitFactory updates

---
+++ database/factories/LotSizeUnitFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\LotSizeUnit;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LotSizeUnitFactory extends Factory
+{
+    protected $model = LotSizeUnit::class;
+
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+}
