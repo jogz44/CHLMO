221aa3a127d8e1e397cd24aed159d42e08bee5a8
pseudocat <sydneyang1234@gmail.com>
2024-10-25 08:35:20 +0800

    feat: working on the adding of occupant

--- app/Livewire/AddNewOccupant.php
+++ app/Livewire/AddNewOccupant.php
@@ -6,6 +6,20 @@ use Livewire\Component;
 
 class AddNewOccupant extends Component
 {
+    // Required fields in adding an occupant
+    public
+        $first_name, $middle_name, $last_name, $suffix_name,
+        $barangay_id, $purok_id, $barangays = [], $puroks = [], $full_address,
+        $civil_status_id, $civilStatuses,
+        $contact_number,
+        $tribe_id, $tribes = [],
+        $sex, $age,
+        $religion_id, $religions = [],
+        $occupation, $monthly_income, $family_income;
+
+    // TODO continue setting the wire:models in the blade file for this. You stopped at 'wire:model="first_name".
+    // TODO Good night, sleep well.
+
     public function render()
     {
         return view('livewire.add-new-occupant');
