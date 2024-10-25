b6d949eeb328dcc8502946e3beb90c74c15100e7
pseudocat <sydneyang1234@gmail.com>
2024-10-25 12:22:44 +0800

    refactor: change some necessary fields in the AddNewOccupant livewire component

--- app/Livewire/AddNewOccupant.php
+++ app/Livewire/AddNewOccupant.php
@@ -10,12 +10,17 @@ class AddNewOccupant extends Component
     public
         $first_name, $middle_name, $last_name, $suffix_name,
         $barangay_id, $purok_id, $barangays = [], $puroks = [], $full_address,
-        $civil_status_id, $civilStatuses,
+        $civil_status_id, $civil_statuses,
         $contact_number,
         $tribe_id, $tribes = [],
-        $sex, $age,
+        $sex, $date_of_birth,
         $religion_id, $religions = [],
         $occupation, $monthly_income, $family_income;
+
+    // Dependent's details
+    public $dependents = [];
+    public $dependent_civil_status_id; // Store selected Civil Status ID
+    public $dependent_civil_statuses; // For populating the civil statuses dropdown
 
     // TODO continue setting the wire:models in the blade file for this. You stopped at 'wire:model="first_name".
     // TODO Good night, sleep well.
