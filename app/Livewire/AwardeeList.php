6e4c4272a4ac610d7623bc78e79c3e4c3f8d6681
pseudocat <sydneyang1234@gmail.com>
2024-10-31 14:46:59 +0800

    feat: display all the awardees in the awardee list table

--- app/Livewire/AwardeeList.php
+++ app/Livewire/AwardeeList.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Awardee;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class AwardeeList extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.awardee-list');
+        $awardees = Awardee::with([
+            'taggedAndValidatedApplicant', 'address', 'lot', 'lotSizeUnit'
+        ])->orderBy('created_at', 'desc')->paginate( 10);
+
+        return view('livewire.awardee-list', [
+            'awardees' => $awardees
+        ]);
     }
 }
