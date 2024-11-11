318cb9c8b0a147203240374fa1c885f6876223d5
pseudocat <sydneyang1234@gmail.com>
2024-11-12 01:22:09 +0800

    refactor: butang kunuhay routes sa api...might delete later

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Added route for the dashboard chart
+Route::get('/chart-data', [DashboardController::class, 'getChartData']);
