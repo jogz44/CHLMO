5bc83e240d9c2abb2ca51339c06a69992bee00f0
pseudocat <sydneyang1234@gmail.com>
2024-11-12 01:22:49 +0800

    style: code cleanup

--- app/Models/Awardee.php
+++ app/Models/Awardee.php
@@ -13,13 +13,7 @@ class Awardee extends Model
     use HasFactory;
 
     protected $fillable = [
-        'tagged_and_validated_applicant_id',
-        'address_id',
-        'relocation_lot_id',
-        'lot_size',
-        'lot_size_unit_id',
-        'grant_date',
-        'is_awarded',
+        'tagged_and_validated_applicant_id', 'address_id','relocation_lot_id','lot_size','lot_size_unit_id','grant_date','is_awarded',
         'is_blacklisted'
     ];
 
