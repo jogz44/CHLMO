61d18c04c5fdacc839d589c77efe0ee733d6c4fc
pseudocat <sydneyang1234@gmail.com>
2024-11-12 01:23:14 +0800

    style: code cleanup

--- app/Models/LivingSituation.php
+++ app/Models/LivingSituation.php
@@ -11,20 +11,10 @@ class LivingSituation extends Model
 {
     use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'living_situation_description',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'id' => 'integer',
     ];
