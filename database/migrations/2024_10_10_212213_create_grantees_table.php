698a866b4eeeb037bfcf4d2f4fd2f38125d558d7
shnmrzpgs <145262229+shnmrzpgs@users.noreply.github.com>
2024-11-13 03:12:32 +0800

    refactor: code cleanup

--- database/migrations/2024_10_10_212213_create_grantees_table.php
+++ database/migrations/2024_10_10_212213_create_grantees_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
         Schema::create('grantees', function (Blueprint $table) {
             $table->id();
             $table->foreignId('profiled_tagged_applicant_id')->constrained('profiled_tagged_applicants')->onDelete('cascade');
-            $table->foreignId('material_id')->constrained('materials')->onDelete('cascade');
-            $table->integer('grantee_quantity'); // For the numeric value
             $table->dateTime('date_of_delivery');
             $table->dateTime('date_of_ris');
             $table->boolean('is_granted')->default(false);
