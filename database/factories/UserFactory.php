105efb091e4f911ef076298d6a3d5194dfca9c6c
pseudocat <sydneyang1234@gmail.com>
2024-11-14 17:05:49 +0800

    refactor: update UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -34,6 +34,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role_id' => fake()->numberBetween(1, 5),
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'remember_token' => Str::random(10),
