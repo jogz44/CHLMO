18d77d369375f1ed2792f990b36ffb2ed8cabc40
pseudocat <sydneyang1234@gmail.com>
2024-11-16 11:09:49 +0800

    feat: add a new migration called 'people' for centralized applicants data

---
+++ database/migrations/2024_09_12_015251_create_people_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 255);
+            $table->string('middle_name', 255)->nullable();
+            $table->string('last_name', 255);
+            $table->string('suffix_name', 255)->nullable();
+            $table->string('contact_number', 15)->nullable();
+            $table->string('application_type', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('people');
+    }
+};
--- database/migrations/2024_09_12_032817_create_applicants_table.php
+++ database/migrations/2024_09_12_032817_create_applicants_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
         Schema::create('applicants', function (Blueprint $table) {
             $table->id();
             $table->string('applicant_id')->unique()->nullable();
+            $table->foreignId('person_id')->constrained('people')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('transaction_type_id')->constrained('transaction_types')->onDelete('cascade');
             $table->foreignId('address_id')->constrained('addresses')->onDelete('cascade');
-            $table->string('first_name', 255);
-            $table->string('middle_name', 255)->nullable();
-            $table->string('last_name', 255);
-            $table->string('suffix_name', 255)->nullable();
-            $table->string('contact_number', 15)->nullable();
             $table->date('date_applied');
             $table->string('initially_interviewed_by', 100);
             $table->boolean('is_tagged')->default(false);
