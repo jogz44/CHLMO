fd3f1ff1fc430a73605f96dc3426f5b1a6eeb079
pseudocat <sydneyang1234@gmail.com>
2024-11-22 01:23:50 +0800

    refactor: some modifications in the routes for trial

--- routes/web.php
+++ routes/web.php
@@ -108,6 +108,10 @@ Route::middleware([
         return view('summary-of-relocation-lot-applicants');
     })->name('summary-of-relocation-lot-applicants');
 
+    Route::get('/masterlist-of-actual-occupants', function () {
+        return view('masterlist-of-actual-occupants');
+    })->name('masterlist-of-actual-occupants');
+
     Route::get('/request-applicant-details', function () {
         return view('request-applicant-details');
     })->name('request-applicant-details');
@@ -219,31 +223,13 @@ Route::middleware([
 });
 
 
-// Admin routes
+// Housing Admin routes
 Route::middleware([
     'auth:web', // Session-based authentication via web guard
-    'role:Admin', // Spatie permission middleware to check if the user has the 'admin' role
+    'role:Housing System Admin', // Spatie permission middleware to check if the user has the 'Housing System' role
     'verified', // Ensures the user has verified their email
 ])->name('admin.')->prefix('admin')->group(function () {
-    Route::get('/', [IndexController::class, 'index'])->name('index');
 
-    Route::resource('/roles', RoleController::class);
-    Route::post('/roles{role}/permissions', [RoleController::class, 'givePermission'])->name('roles.permissions');
-    Route::delete('/roles{role}/permissions/{permission}', [RoleController::class, 'revokePermission'])->name('roles.permissions.revoke');
-
-    Route::resource('/permissions', PermissionController::class);
-    Route::post('/permissions/{permission}/roles', [PermissionController::class, 'assignRole'])->name('permissions.roles');
-    Route::delete('/permissions{permission}/roles/{role}', [PermissionController::class, 'removeRole'])->name('permissions.roles.remove');
-
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-
-    Route::post('/users/{user}/roles', [UserController::class, 'assignRole'])->name('users.roles');
-    Route::delete('/users/{user}/roles/{role}', [UserController::class, 'removeRole'])->name('users.roles.remove');
-
-    Route::post('/users/{user}/permissions', [UserController::class, 'givePermission'])->name('users.permissions');
-    Route::delete('/users/{user}/permissions/{permission}', [UserController::class, 'revokePermission'])->name('users.permissions.revoke');
 });
 
 // Shelter Admin routes
