d5e2ea17f3bc806be7d1867ce163959843c2a0fe
pseudocat <sydneyang1234@gmail.com>
2024-11-21 20:46:22 +0800

    style: Applicant model code cleanup

--- app/Models/Applicant.php
+++ app/Models/Applicant.php
@@ -31,24 +31,6 @@ class Applicant extends Model
         'date_applied' => 'date',
     ];
 
-//    public static function generateApplicantId(): string
-//    {
-//        $currentYear = Carbon::now()->year;
-//
-//        // Increment the count for the current year
-//        $countForYear = ApplicantCounter::incrementCountForYear($currentYear);
-//
-//        // Format the ID: "YYYY-000XXX"
-//        $applicantId = sprintf('%d-%06d', $currentYear, $countForYear);
-//
-//        // Log the generated values for debugging
-//        logger()->info('Generating applicant ID', [
-//            'year' => $currentYear,
-//            'count' => $countForYear
-//        ]);
-//
-//        return $applicantId;
-//    }
     public static function generateApplicantId(): string
     {
         $currentYear = Carbon::now()->year;
