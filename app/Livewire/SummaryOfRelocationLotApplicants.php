3aa538c4714a2dfb1ac076d0cf59593385d8589e
pseudocat <sydneyang1234@gmail.com>
2024-11-21 12:35:21 +0800

    feat: livewire component for new report: summary-of-relocation-lot-applicants

---
+++ app/Livewire/SummaryOfRelocationLotApplicants.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SummaryOfRelocationLotApplicants extends Component
+{
+    public function render()
+    {
+        return view('livewire.summary-of-relocation-lot-applicants');
+    }
+}
