bb35d6690d789b23a284665c0e81d668cfbfc191
Metanoia-missno <ngppalmes00206@usep.edu.ph>
2024-11-26 11:01:21 +0800

    feat: Developers Page

--- resources/views/activity-logs.blade.php
+++ resources/views/activity-logs.blade.php
@@ -1,18 +1,25 @@
 <x-app-layout>
-    <div x-data="{ openFilters: false }" class="p-10  ml-[17%] mt-[60px]">
+    <div x-data="{ openFilters: false }" class="p-10  ml-[17%] mt-[100px]">
         <div class="flex bg-gray-100 text-[12px]">
             <!-- Main Content -->
             <div x-data="pagination()" class="flex-1 p-6 overflow-auto">
                 <div class="bg-white rounded shadow mb-4 flex items-center justify-between z-0 relative p-3">
                     <div class="flex items-center">
-                        <h2 class="text-[13px] ml-5 text-gray-700">ACTIVITY LOGS</h2>
+                        <h2 class="text-[13px] ml-5 text-gray-700">
+                            <a href="{{ route('activity-logs') }}" class="text-black hover:underline">ACTIVITY LOGS</a>
+                        </h2>
+                        <h2 class="text-[13px] ml-5 text-gray-700">
+                            <a href="{{ route('developers') }}" class="text-black hover:underline">DEVELOPERS</a>
+                        </h2>
+
+
                     </div>
                     <img src="{{ asset('storage/images/design.png') }}" alt="Design" class="absolute right-0 top-0 h-full object-cover opacity-100 z-0">
                     <div x-data class="relative z-0">
                         <button class="bg-custom-green text-white px-4 py-2 rounded">Export</button>
                     </div>
                 </div>
-                
+
                 <div class="bg-white p-6 rounded shadow">
                     <div class="flex justify-between items-center">
                         <div class="flex space-x-2">
@@ -29,17 +36,11 @@
                             </button>
                             <!-- Search -->
                             <div class="relative hidden md:block border-gray-300">
-                                <svg class="absolute top-[13px] left-4" width="19" height="19" viewBox="0 0 21 21"
-                                    fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M9.625 16.625C13.491 16.625 16.625 13.491 16.625 9.625C16.625 5.75901 13.491 2.625 9.625 2.625C5.75901 2.625 2.625 5.75901 2.625 9.625C2.625 13.491 5.75901 16.625 9.625 16.625Z"
-                                        stroke="#787C7F" stroke-width="1.75" stroke-linecap="round"
-                                        stroke-linejoin="round" />
-                                    <path d="M18.3746 18.375L14.5684 14.5688" stroke="#787C7F" stroke-width="1.75"
-                                        stroke-linecap="round" stroke-linejoin="round" />
+                                <svg class="absolute top-[13px] left-4" width="19" height="19" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M9.625 16.625C13.491 16.625 16.625 13.491 16.625 9.625C16.625 5.75901 13.491 2.625 9.625 2.625C5.75901 2.625 2.625 5.75901 2.625 9.625C2.625 13.491 5.75901 16.625 9.625 16.625Z" stroke="#787C7F" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M18.3746 18.375L14.5684 14.5688" stroke="#787C7F" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" />
                                 </svg>
-                                <input type="search" name="search" id="search"
-                                    class="rounded-md px-12 py-2 placeholder:text-[13px] z-10 border border-gray-300 bg-[#f7f7f9] hover:ring-custom-yellow focus:ring-custom-yellow"
-                                    placeholder="Search">
+                                <input type="search" name="search" id="search" class="rounded-md px-12 py-2 placeholder:text-[13px] z-10 border border-gray-300 bg-[#f7f7f9] hover:ring-custom-yellow focus:ring-custom-yellow" placeholder="Search">
                             </div>
                         </div>
                     </div>
@@ -126,24 +127,15 @@
 
                 <!-- Pagination controls -->
                 <div class="flex justify-end text-[12px] mt-4">
-                    <button
-                        @click="prevPage"
-                        :disabled="currentPage === 1"
-                        class="px-4 py-2 bg-gray-300 text-gray-700 rounded-l disabled:opacity-50">
+                    <button @click="prevPage" :disabled="currentPage === 1" class="px-4 py-2 bg-gray-300 text-gray-700 rounded-l disabled:opacity-50">
                         Prev
                     </button>
                     <template x-for="page in totalPages" :key="page">
-                        <button
-                            @click="goToPage(page)"
-                            :class="{'bg-custom-green text-white': page === currentPage, 'bg-gray-200': page !== currentPage}"
-                            class="px-4 py-2 mx-1 rounded">
+                        <button @click="goToPage(page)" :class="{'bg-custom-green text-white': page === currentPage, 'bg-gray-200': page !== currentPage}" class="px-4 py-2 mx-1 rounded">
                             <span x-text="page"></span>
                         </button>
                     </template>
-                    <button
-                        @click="nextPage"
-                        :disabled="currentPage === totalPages"
-                        class="px-4 py-2 bg-gray-300 text-gray-700 rounded-r disabled:opacity-50">
+                    <button @click="nextPage" :disabled="currentPage === totalPages" class="px-4 py-2 bg-gray-300 text-gray-700 rounded-r disabled:opacity-50">
                         Next
                     </button>
                 </div>
