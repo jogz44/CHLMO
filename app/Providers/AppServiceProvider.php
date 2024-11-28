c0445d1cd1508f9405abc127c3a608e5ac847745
pseudocat <sydneyang1234@gmail.com>
2024-11-29 07:47:48 +0800

    refactor: modify AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use App\Models\Awardee;
 use App\Observers\AwardeeObserver;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Log;
 
@@ -26,15 +28,8 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useTailwind();
         Awardee::observe(AwardeeObserver::class);
 
-        if (!file_exists(public_path('tagging-house-structure-images'))) {
-            try {
-                symlink(
-                    storage_path('app/tagging-house-structure-images'),
-                    public_path('tagging-house-structure-images')
-                );
-            } catch (\Exception $e) {
-                Log::error('Failed to create symbolic link: ' . $e->getMessage());
-            }
-        }
+        Storage::disk('tagging-house-structure-images')->buildTemporaryUrlsUsing(function ($path, $expiration, $options) {
+            return URL::to('storage/tagging-house-structure-images/'.$path);
+        });
     }
 }
