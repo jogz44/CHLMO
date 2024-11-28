1cd32b598a855d601fa73063887a19defb89e90b
pseudocat <sydneyang1234@gmail.com>
2024-11-28 11:17:21 +0800

    refactor: models update

--- app/Models/TaggedAndValidatedApplicant.php
+++ app/Models/TaggedAndValidatedApplicant.php
@@ -18,7 +18,7 @@ class TaggedAndValidatedApplicant extends Model
         'wall_type_id', 'structure_status_id', 'relocation_lot_id', 'full_address', 'sex', 'date_of_birth', 'occupation',
         'monthly_income', 'tagging_date', 'room_rent_fee', 'room_landlord', 'house_rent_fee', 'house_landlord',
         'lot_rent_fee', 'lot_landlord', 'house_owner', 'relationship_to_house_owner', 'tagger_name',
-        'years_of_residency', 'remarks', 'is_tagged', 'is_awarding_on_going',
+        'years_of_residency', 'voters_id_number', 'remarks', 'is_tagged', 'is_awarding_on_going',
     ];
 
     protected $casts = [
