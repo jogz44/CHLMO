c592baa02bba82f92094103d6d81feb6522934f0
shnmrzpgs <145262229+shnmrzpgs@users.noreply.github.com>
2024-12-05 12:23:23 +0800

    style: style changes for export for header reports

--- resources/views/exports/shelter-request-delivered-materials.blade.php
+++ resources/views/exports/shelter-request-delivered-materials.blade.php
@@ -1,31 +1,46 @@
 <table>
     <!-- Spacing -->
     <tr>
-        <td colspan="5" height="81.6"></td>
+        <td colspan="5" height="20"></td>
     </tr>
     <tr>
-        <td colspan="5" style="text-align: center; margin-top: 40px; font-size: 9px; font-weight: bold;">
+        <td colspan="5" style="text-align: center; margin-top: 40px; font-size: 11px; font-weight: bold;">
             Republic of the Philippines
         </td>
     </tr>
     <tr>
-        <td colspan="5" style="text-align: center; font-size: 9px; font-weight: bold;">
+        <td colspan="5" style="text-align: center; font-size: 11px; font-weight: bold;">
             Province of Davao del Norte
         </td>
     </tr>
     <tr>
-        <td colspan="5" style="text-align: center; font-size: 9px; font-weight: bold;">
-            City of Tagum
-        </td>
-    </tr>
-    <tr>
         <td colspan="5" style="text-align: center; font-size: 11px; font-weight: bold;">
-            CITY HOUSING AND LAND MANAGEMENT OFFICE
+            CITY OF TAGUM
         </td>
     </tr>
-
-    <!-- Spacing -->
     <tr>
+        <td colspan="5"></td>
+    </tr>
+    <tr>
+        <td colspan="5" style="text-align: center; font-size: 9px; font-weight: bold;">
+            OFFICE OF THE CITY HOUSING AND LAND MANAGEMENT OFFICER
+        </td>
+    </tr>
+    <tr>
+        <td colspan="5" style="text-align: center; font-size: 9px; font-weight: regular;">
+            2/F Annex Building, City Government, J.V. Ayala Ave., Barangay Apokon, Tagum City
+        </td>
+    </tr>
+    <tr>
+        <td colspan="5" style="text-align: center; font-size: 9px; font-weight: regular;">
+            Telephone No. (9) 216 - 9367 Local 141
+        </td>
+
+        <!-- Spacing -->
+    <tr style="border-bottom: 4px solid #000;">
+        <td colspan="5" height="10"></td>
+    </tr>
+    <tr style="border-bottom: 4px solid #000;">
         <td colspan="5" height="20"></td>
     </tr>
 
