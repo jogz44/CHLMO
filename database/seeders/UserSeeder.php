d9118f98ff55aaeb488382fe2a24b240eff69ba2
pseudocat <sydneyang1234@gmail.com>
2024-12-06 09:18:29 +0800

    refactor: user management updates

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,29 +14,29 @@ class UserSeeder extends Seeder
 
     public function run(): void
     {
-        User::create([
-            'username' => 'admin_1',
-            'first_name' => 'Sheldon',
-            'middle_name' => 'Shelly',
-            'last_name' => 'Cooper',
-            'password' => Hash::make('password'),
-            'is_disabled' => false, // Explicitly set this
-            'two_factor_secret' => null,
-            'two_factor_recovery_codes' => null,
-            'profile_photo_path' => null,
-        ])->assignRole('Housing System Admin');
+//        User::create([
+//            'username' => 'admin_1',
+//            'first_name' => 'Sheldon',
+//            'middle_name' => 'Shelly',
+//            'last_name' => 'Cooper',
+//            'password' => Hash::make('password'),
+//            'is_disabled' => false, // Explicitly set this
+//            'two_factor_secret' => null,
+//            'two_factor_recovery_codes' => null,
+//            'profile_photo_path' => null,
+//        ])->assignRole('Housing System Admin');
 
-        User::create([
-            'username' => 'housingAdmin_1',
-            'first_name' => 'Sabrina',
-            'middle_name' => 'Car',
-            'last_name' => 'Painter',
-            'password' => Hash::make('password'), // Make consistent with admin
-            'is_disabled' => false, // Explicitly set this
-            'two_factor_secret' => null,
-            'two_factor_recovery_codes' => null,
-            'profile_photo_path' => null,
-        ])->assignRole('Shelter System Admin');
+//        User::create([
+//            'username' => 'housingAdmin_1',
+//            'first_name' => 'Sabrina',
+//            'middle_name' => 'Car',
+//            'last_name' => 'Painter',
+//            'password' => Hash::make('password'), // Make consistent with admin
+//            'is_disabled' => false, // Explicitly set this
+//            'two_factor_secret' => null,
+//            'two_factor_recovery_codes' => null,
+//            'profile_photo_path' => null,
+//        ])->assignRole('Shelter System Admin');
 
         User::create([
             'username' => 'super_admin',
